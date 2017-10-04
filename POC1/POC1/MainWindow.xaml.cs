61e59e08de7d70755ac7246b432d9fcea632a647
Legault, Miguel <8547102@johnabbottcollege.net>
2017-10-04 15:00:06 -0400

    POC1 roundtrip diff patch works

--- POC1/POC1/MainWindow.xaml.cs
+++ POC1/POC1/MainWindow.xaml.cs
@@ -59,6 +59,32 @@ namespace POC1
             updateCommand.Parameters.Add(new SqlParameter("NewXaml", XamlContent));
             updateCommand.ExecuteNonQuery();
         }
+        public void InsertNewChange(string diff)
+        {
+            // Insert
+            SqlCommand insertCommand = new SqlCommand("INSERT INTO Changes (ChangeInstructions) VALUES (@diffContent)", conn);
+            insertCommand.Parameters.Add(new SqlParameter("diffContent", diff));
+            insertCommand.ExecuteNonQuery();
+
+        }
+        public string GetDocChange(int Id)
+        {
+            string changeContent = "";
+            // Select
+            SqlCommand command = new SqlCommand("SELECT * FROM Changes WHERE ID=@Id", conn);
+            command.Parameters.Add(new SqlParameter("Id", Id));
+
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    changeContent += reader["ChangeInstructions"];
+                }
+            }
+            return changeContent;
+        }
+
     }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -68,8 +94,56 @@ namespace POC1
         private Timer tmUpdate;
         private SynchEdDB DBSynched;
 
+        string PreviousVersionOfContent = ""; // We'll perform the diff against this value
+
 
         private bool IsListening = true; // Boolean to indicate role of instance. Defaults to true. false means updating
+
+        public string GenerateDiffGram(string originalversion, string changedversion)
+        {
+            // Make xml readers out of strings
+            XmlReader srOriginalVersion = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(originalversion)));
+            XmlReader srChangedVersion = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(changedversion)));
+
+            MemoryStream msDiffOutput = new MemoryStream();
+            XmlWriter diffGramWriter = XmlWriter.Create(msDiffOutput);
+
+            string diffGram = "";
+
+            XmlDiff xmldiff = new XmlDiff(XmlDiffOptions.IgnoreNamespaces |
+                                             XmlDiffOptions.IgnorePrefixes);
+            bool bIdentical = xmldiff.Compare(srOriginalVersion, srChangedVersion, diffGramWriter);
+            diffGramWriter.Close();
+
+            // Get a string from stream
+            diffGram = Encoding.UTF8.GetString(msDiffOutput.ToArray());
+            return diffGram;
+        }
+
+        public string Patch(string originalversion, string diffContent)
+        {
+            string newVersion = "";
+
+            XmlDocument sourceDoc = new XmlDocument(new NameTable());
+            XmlReader srOriginalVersion = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(originalversion)));
+            XmlReader srDiffContent = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(diffContent)));
+
+            sourceDoc.Load(srOriginalVersion);
+
+            XmlPatch myPatcher = new XmlPatch();
+            myPatcher.Patch(sourceDoc, srDiffContent);
+
+            MemoryStream msNewVersionOutput = new MemoryStream();
+            XmlWriter newVersionWriter = XmlWriter.Create(msNewVersionOutput);
+
+            sourceDoc.Save(newVersionWriter);
+            newVersionWriter.Close();
+
+            newVersion = Encoding.UTF8.GetString(msNewVersionOutput.ToArray());
+
+            return newVersion;
+        }
+
 
         public MainWindow()
         {
@@ -127,6 +201,41 @@ namespace POC1
             MemoryStream stream = new MemoryStream();
             TextRange range = new TextRange(rtbMainDoc.Document.ContentStart, rtbMainDoc.Document.ContentEnd);
             stream = new MemoryStream(Encoding.UTF8.GetBytes(XamlContent));
+            range.Load(stream, System.Windows.DataFormats.Xaml);
+
+            stream.Close();
+
+            // Also keep a copy
+            range = new TextRange(rtbMainDoc.Document.ContentStart, rtbMainDoc.Document.ContentEnd);
+            stream = new MemoryStream();
+            range.Save(stream, DataFormats.Xaml); // Format might be Xaml
+            PreviousVersionOfContent = Encoding.UTF8.GetString(stream.ToArray());
+            stream.Close();
+
+
+        }
+
+        private void btnSendChange_Click(object sender, RoutedEventArgs e)
+        {
+            // Get content as a string
+            TextRange range = new TextRange(rtbMainDoc.Document.ContentStart, rtbMainDoc.Document.ContentEnd);
+            MemoryStream stream = new MemoryStream();
+            range.Save(stream, DataFormats.Xaml); // Format might be Xaml
+            string newVersion = Encoding.UTF8.GetString(stream.ToArray());
+
+            string strDiff = GenerateDiffGram(PreviousVersionOfContent, newVersion);
+
+            DBSynched.InsertNewChange(strDiff);
+
+        }
+
+        private void btnGetChanges_Click(object sender, RoutedEventArgs e)
+        {
+            string newVersion = Patch(PreviousVersionOfContent, DBSynched.GetDocChange(3));
+
+            MemoryStream stream = new MemoryStream();
+            TextRange range = new TextRange(rtbMainDoc.Document.ContentStart, rtbMainDoc.Document.ContentEnd);
+            stream = new MemoryStream(Encoding.UTF8.GetBytes(newVersion));
             range.Load(stream, System.Windows.DataFormats.Xaml);
             stream.Close();
 
