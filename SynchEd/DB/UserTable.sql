6bda2ec7aa6262f8e6a179ca1fe51773290a5e7b
Legault, Miguel <8547102@johnabbottcollege.net>
2017-10-16 16:07:20 -0400

    SynchEd Deploy Created a valid listwith actual colleagues SynchEd renewed deployment certificates

--- SynchEd/DB/UserTable.sql
+++ SynchEd/DB/UserTable.sql
@@ -6,8 +6,25 @@
 )
 
 INSERT INTO [dbo].[User] (Name,UserKey) VALUES 
-('Suzan','UUU1'),
-('Bert','UUU2')
+('Alex','UUU1'),
+('Arturo','UUU2'),
+('Azam','UUU3'),
+('Derek','UUU4'),
+('Giorgio','UUU5'),
+('Gregory','UUU6'),
+('Hana','UUU7'),
+('Hossein','UUU8'),
+('Lucas','UUU9'),
+('Miguel','UU10'),
+('Miho','UU11'),
+('Mohamed','UU12'),
+('Mohammadjavad','UU13'),
+('Mojgan','UU14'),
+('Olesksii','UU15'),
+('Roberto','UU18'),
+('Vajihehalsadat','UU16'),
+('Winoto','UU17'),
+('Default User','UUUZ')
 ;
 
 SELECT * FROM [dbo].[User] WHERE UserKey='UUUZ'
